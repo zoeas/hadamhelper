ea01c1217cb8a07832d93f7c375adc6d62369216
hadam <hadamch@naver.com>
2016-02-07 17:21:23 +0900

    ls9 도우미 형태랑 로직의 토대는 완성 하지만 버튼의 작동이 뒤바뀌는 버그가 있음. 핸들할당을 순차적으로하는데 거기에 문제가 있는듯

---
+++ hadam_ls9helper/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace hadam_ls9helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_pMic = new System.Windows.Forms.Button();
+            this.btn_cMic = new System.Windows.Forms.Button();
+            this.btn_downMain = new System.Windows.Forms.Button();
+            this.btn_wMic = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cBox_wMic4 = new System.Windows.Forms.CheckBox();
+            this.cBox_wMic3 = new System.Windows.Forms.CheckBox();
+            this.cBox_wMic2 = new System.Windows.Forms.CheckBox();
+            this.cBox_wMic1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cBox_wMics = new System.Windows.Forms.CheckBox();
+            this.cBox_cMic = new System.Windows.Forms.CheckBox();
+            this.cBox_pMic = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_pMic
+            // 
+            this.btn_pMic.Location = new System.Drawing.Point(161, 17);
+            this.btn_pMic.Name = "btn_pMic";
+            this.btn_pMic.Size = new System.Drawing.Size(106, 86);
+            this.btn_pMic.TabIndex = 0;
+            this.btn_pMic.Text = "관현악";
+            this.btn_pMic.UseVisualStyleBackColor = true;
+            this.btn_pMic.Click += new System.EventHandler(this.btn_pMic_Click);
+            // 
+            // btn_cMic
+            // 
+            this.btn_cMic.Location = new System.Drawing.Point(161, 119);
+            this.btn_cMic.Name = "btn_cMic";
+            this.btn_cMic.Size = new System.Drawing.Size(106, 86);
+            this.btn_cMic.TabIndex = 1;
+            this.btn_cMic.Text = "찬양대";
+            this.btn_cMic.UseVisualStyleBackColor = true;
+            this.btn_cMic.Click += new System.EventHandler(this.btn_cMic_Click);
+            // 
+            // btn_downMain
+            // 
+            this.btn_downMain.Location = new System.Drawing.Point(25, 17);
+            this.btn_downMain.Name = "btn_downMain";
+            this.btn_downMain.Size = new System.Drawing.Size(106, 86);
+            this.btn_downMain.TabIndex = 3;
+            this.btn_downMain.Text = "아랫강대상";
+            this.btn_downMain.UseVisualStyleBackColor = true;
+            this.btn_downMain.Click += new System.EventHandler(this.btn_downMain_Click);
+            // 
+            // btn_wMic
+            // 
+            this.btn_wMic.Location = new System.Drawing.Point(25, 119);
+            this.btn_wMic.Name = "btn_wMic";
+            this.btn_wMic.Size = new System.Drawing.Size(106, 86);
+            this.btn_wMic.TabIndex = 4;
+            this.btn_wMic.Text = "무선 마이크";
+            this.btn_wMic.UseVisualStyleBackColor = true;
+            this.btn_wMic.Click += new System.EventHandler(this.btn_wMic_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_wMic);
+            this.panel1.Controls.Add(this.btn_pMic);
+            this.panel1.Controls.Add(this.btn_downMain);
+            this.panel1.Controls.Add(this.btn_cMic);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 221);
+            this.panel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cBox_wMic4);
+            this.groupBox1.Controls.Add(this.cBox_wMic3);
+            this.groupBox1.Controls.Add(this.cBox_wMic2);
+            this.groupBox1.Controls.Add(this.cBox_wMic1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 119);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "무선 마이크";
+            // 
+            // cBox_wMic4
+            // 
+            this.cBox_wMic4.AutoSize = true;
+            this.cBox_wMic4.Location = new System.Drawing.Point(25, 90);
+            this.cBox_wMic4.Name = "cBox_wMic4";
+            this.cBox_wMic4.Size = new System.Drawing.Size(70, 16);
+            this.cBox_wMic4.TabIndex = 3;
+            this.cBox_wMic4.Text = "무선 4번";
+            this.cBox_wMic4.UseVisualStyleBackColor = true;
+            // 
+            // cBox_wMic3
+            // 
+            this.cBox_wMic3.AutoSize = true;
+            this.cBox_wMic3.Location = new System.Drawing.Point(25, 67);
+            this.cBox_wMic3.Name = "cBox_wMic3";
+            this.cBox_wMic3.Size = new System.Drawing.Size(70, 16);
+            this.cBox_wMic3.TabIndex = 2;
+            this.cBox_wMic3.Text = "무선 3번";
+            this.cBox_wMic3.UseVisualStyleBackColor = true;
+            // 
+            // cBox_wMic2
+            // 
+            this.cBox_wMic2.AutoSize = true;
+            this.cBox_wMic2.Location = new System.Drawing.Point(25, 44);
+            this.cBox_wMic2.Name = "cBox_wMic2";
+            this.cBox_wMic2.Size = new System.Drawing.Size(70, 16);
+            this.cBox_wMic2.TabIndex = 1;
+            this.cBox_wMic2.Text = "무선 2번";
+            this.cBox_wMic2.UseVisualStyleBackColor = true;
+            // 
+            // cBox_wMic1
+            // 
+            this.cBox_wMic1.AutoSize = true;
+            this.cBox_wMic1.Location = new System.Drawing.Point(25, 21);
+            this.cBox_wMic1.Name = "cBox_wMic1";
+            this.cBox_wMic1.Size = new System.Drawing.Size(70, 16);
+            this.cBox_wMic1.TabIndex = 0;
+            this.cBox_wMic1.Text = "무선 1번";
+            this.cBox_wMic1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cBox_wMics);
+            this.groupBox2.Controls.Add(this.cBox_cMic);
+            this.groupBox2.Controls.Add(this.cBox_pMic);
+            this.groupBox2.Location = new System.Drawing.Point(151, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(131, 119);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "연결";
+            // 
+            // cBox_wMics
+            // 
+            this.cBox_wMics.AutoSize = true;
+            this.cBox_wMics.Location = new System.Drawing.Point(20, 87);
+            this.cBox_wMics.Name = "cBox_wMics";
+            this.cBox_wMics.Size = new System.Drawing.Size(72, 16);
+            this.cBox_wMics.TabIndex = 2;
+            this.cBox_wMics.Text = "마이크들";
+            this.cBox_wMics.UseVisualStyleBackColor = true;
+            // 
+            // cBox_cMic
+            // 
+            this.cBox_cMic.AutoSize = true;
+            this.cBox_cMic.Location = new System.Drawing.Point(20, 57);
+            this.cBox_cMic.Name = "cBox_cMic";
+            this.cBox_cMic.Size = new System.Drawing.Size(60, 16);
+            this.cBox_cMic.TabIndex = 1;
+            this.cBox_cMic.Text = "찬양대";
+            this.cBox_cMic.UseVisualStyleBackColor = true;
+            // 
+            // cBox_pMic
+            // 
+            this.cBox_pMic.AutoSize = true;
+            this.cBox_pMic.Location = new System.Drawing.Point(20, 27);
+            this.cBox_pMic.Name = "cBox_pMic";
+            this.cBox_pMic.Size = new System.Drawing.Size(100, 16);
+            this.cBox_pMic.TabIndex = 0;
+            this.cBox_pMic.Text = "관현악 마이크";
+            this.cBox_pMic.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 221);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(294, 146);
+            this.panel2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 367);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_pMic;
+        private System.Windows.Forms.Button btn_cMic;
+        private System.Windows.Forms.Button btn_downMain;
+        private System.Windows.Forms.Button btn_wMic;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cBox_wMic4;
+        private System.Windows.Forms.CheckBox cBox_wMic3;
+        private System.Windows.Forms.CheckBox cBox_wMic2;
+        private System.Windows.Forms.CheckBox cBox_wMic1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cBox_wMics;
+        private System.Windows.Forms.CheckBox cBox_cMic;
+        private System.Windows.Forms.CheckBox cBox_pMic;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ hadam_ls9helper/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hadam_ls9helper
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowNmae);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr param);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWind, StringBuilder className, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindow(IntPtr hWnd, int uCmd);
+        private const int GW_HWNDFIRST = 0; // 젤 첨음
+        private const int GW_HWNDLAST = 1;  // 마지막
+        private const int GW_HWNDNEXT = 2;  // 바로 다음
+        private const int GW_HWNDPREV = 3;  // 바로 이전
+        private const int GW_OWNER = 4;     // 부모
+        private const int GW_CHILD = 5;     // 첫번째 자식
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP = 0x0202;
+        const int BM_CLICK = 0x00F5;
+
+        private delegate bool EnumWindowProc(IntPtr childHandle, IntPtr pointer);
+        private List<IntPtr> _chSetHndList = new List<IntPtr>();
+        private IntPtr _chOnOff;
+        private IntPtr _chSong;
+        private IntPtr _chWMic1;
+        private IntPtr _chWMic2;
+        private IntPtr _chWMic3;
+        private IntPtr _chWMic4;
+        private IntPtr _chPiano;
+        private IntPtr _chDownMain;
+
+
+        /// <summary>
+        /// 최상위 부모 프로세스와 자식 프로세스중 필요한 부분 일부는
+        /// 이름이 특정되어있고 이후 필요한 자식프로세스들은 이름과 클래스가 전부 중복되어있다
+        /// 그러므로 일단 부모 프로세스를 잡고
+        /// 그 아래를 EnumChildWindows를 검색하여 잡는다 (두개)
+        /// 각각 32-17, 16-1 번까지의 번호를 가지고 있다
+        /// 이들은 전부 클래스와 캡션이 중복되므로 순서에 의해서 찾아야된다
+        /// 즉 1번을 찾을려면 두번째 것의 가장 마지막 16번째 자식이 해당된다
+        /// 그리고 거기서 이제 캡션으로 (onOfOn)을 찾으면 된다
+        /// </summary>
+        /// <param name="childHandle"></param>
+        /// <param name="pointer"></param>
+        /// <returns></returns>
+        private bool GetChildHandler(IntPtr childHandle, IntPtr pointer)
+        {
+            StringBuilder sb = new StringBuilder();
+            GetWindowText(childHandle, sb, sb.Capacity);
+            if(sb.ToString().Equals("qt_viewport"))
+            {
+                _chSetHndList.Add(GetWindow(childHandle, GW_CHILD));     // 자식의 자식이 채널세트, 32, 16에 대한 핸들을 저장
+            }
+            return true;
+        }
+
+        // 최종적으로 enumchild 한번 더 돌려야된다
+        private bool GetChOnOffHandler(IntPtr onOff, IntPtr param)
+        {
+            StringBuilder sb = new StringBuilder();
+            GetWindowText(onOff, sb, sb.Capacity);
+            if(sb.ToString().Equals("onofOn"))
+            {
+                _chOnOff = onOff;
+                return false;           // 정확히 찾아낸후 멈춘다
+            }
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            FindChSetHnd();
+            FindChHnd();
+        }
+
+        private void FindChSetHnd()
+        {
+            IntPtr handle = FindWindow(null, "LS9 2");  // 최상위 핸들 찾고
+            if(handle != null)
+            {
+                EnumChildWindows(handle, GetChildHandler, IntPtr.Zero); // 자식들 검색해서 채널세트 32,16을 찾아낸다
+            }
+        }
+
+
+        /// <summry>
+        /// 원하는 채널은 
+        /// 32 - 찬양대
+        /// 21 - 무선 4
+        /// 20 - 무선 3
+        /// 19 - 무선 2
+        /// 18 - 무선 1
+        /// 16 - 피아노
+        /// 8 - 아랫강대상
+        /// 일곱개 채널이다. 32채널세트에서 무선1까지, 나머지 두개는 16채널세트에서 찾아낸다
+        /// </summary>
+        private void FindChHnd()
+        {
+            // 각채널세트의 첫번째 채널들
+            IntPtr ch32 = GetWindow(_chSetHndList[0], GW_CHILD);
+            IntPtr ch16 = GetWindow(_chSetHndList[1], GW_CHILD);
+
+            //32, 21, 20, 19, 18
+            EnumChildWindows(ch32, GetChOnOffHandler, IntPtr.Zero);
+            _chSong = _chOnOff;
+
+            IntPtr ch21 = ChSelector(9, ch32);
+            EnumChildWindows(ch21, GetChOnOffHandler, IntPtr.Zero);
+            _chWMic1 = _chOnOff;
+
+            IntPtr ch20 = ChSelector(1, ch21);
+            EnumChildWindows(ch20, GetChOnOffHandler, IntPtr.Zero);
+            _chWMic2 = _chOnOff;
+
+            IntPtr ch19 = ChSelector(1, ch20);
+            EnumChildWindows(ch19, GetChOnOffHandler, IntPtr.Zero);
+            _chWMic3 = _chOnOff;
+
+            IntPtr ch18 = ChSelector(1, ch19);
+            EnumChildWindows(ch18, GetChOnOffHandler, IntPtr.Zero);
+            _chWMic4 = _chOnOff;
+
+            // 16, 8
+            EnumChildWindows(ch16, GetChOnOffHandler, IntPtr.Zero);
+            _chPiano = _chOnOff;
+
+            IntPtr ch8 = ChSelector(8, ch16);
+            EnumChildWindows(ch8, GetChOnOffHandler, IntPtr.Zero);
+            _chDownMain = _chOnOff;
+
+        }
+
+        // 주어진 숫자만큼 프로세서를 이동한다. 예를들어 32채널에서 29채널로 가고 싶다면 3 이동하면된다
+        private IntPtr ChSelector(int mov, IntPtr wHnd)
+        {
+            for(int i= 0; i< mov; i++)
+            {
+                wHnd = GetWindow(wHnd, GW_HWNDNEXT);
+            }
+            return wHnd;
+        }
+
+        private void btn_downMain_Click(object sender, EventArgs e)
+        {
+            SendMessage(_chDownMain, WM_LBUTTONDOWN, 0, 0);
+            SendMessage(_chDownMain, WM_LBUTTONUP, 0, 0);
+        }
+
+        private void btn_pMic_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(_chPiano.ToString());
+            SendMessage(_chPiano, WM_LBUTTONDOWN, 0, 0);
+            SendMessage(_chPiano, WM_LBUTTONUP, 0, 0);
+        }
+
+        private void btn_wMic_Click(object sender, EventArgs e)
+        {
+            SendMessage(_chWMic1, WM_LBUTTONDOWN, 0, 0);
+            SendMessage(_chWMic1, WM_LBUTTONUP, 0, 0);
+        }
+
+        private void btn_cMic_Click(object sender, EventArgs e)
+        {
+            SendMessage(_chSong, WM_LBUTTONDOWN, 0, 0);
+            SendMessage(_chSong, WM_LBUTTONUP, 0, 0);
+        }
+    }
+}
